89df5aaa8c175b4ad81422457d9e6eb3d5ceb409
clément ALEXANDRE <clement.alexandre0207@gmail.com>
2024-05-15 11:37:12 +0200

    ajout header

---
+++ src/Components/accueil.jsx
@@ -0,0 +1,12 @@
+
+function AccueilIUT() {
+  return (
+    <>
+        <div className="container">
+            <h1>Bienvenue sur IUT-note</h1>
+        </div>
+    </>
+  );
+}
+
+export default AccueilIUT;

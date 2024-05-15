16c3e7b267ed7e57ddf2adf59a21b7fe355ba6d8
clément ALEXANDRE <clement.alexandre0207@gmail.com>
2024-05-15 12:03:15 +0200

    page accueil

--- src/Components/accueil.jsx
+++ src/Components/accueil.jsx
@@ -1,11 +1,21 @@
+import './accueil.css';
 
 function AccueilIUT() {
   return (
     <>
         <div className="container mt-4 mb-4 text-center">
             <h1>Bienvenue sur IUT-note</h1>
-            <img src="./src/assets/media/logo-IUT-blanc.png" alt="IUT" className="img-fluid" />
         </div>
+        <div className="container mt-4 mb-4 text-center">
+            <h2>Informations de contact</h2>
+            <p>Avenue des Facultés</p>
+            <p>- Le Bailly -</p>
+            <p> 80025 AMIENS Cedex 1</p>
+            <p></p>
+            <p>Tél. +33(0)3.22.53.40.40</p>
+            <p>Fax. +33(0)3.22.89.66.33</p>
+        </div>
+        <img src="./src/assets/media/logo-IUT.png" alt="IUT" className="bottom-right-image" />
     </>
   );
 }

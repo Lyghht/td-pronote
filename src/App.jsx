cd9b5d7aa52429696df73e68bd9ec207f716d48d
Nathan <nathan.nlid@gmail.com>
2024-05-15 11:48:47 +0200

    Ajout des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from 'react'
 //import './App.css'
 import AccueilIUT from './Components/accueil'
@@ -7,8 +8,15 @@ function App() {
 
   return (
     <>
-      <Header />
-      <AccueilIUT />
+    <Header />
+    <Router>
+      
+    </Router>
+      <Route path="/" element={<AccueilIUT />}/>
+      <Route path="/etudiants" element={<Etudiants />}/>
+{/*       <Route path="/enseignants" element={<Enseignants />}/> */}
+{/*       <Route path="/notes" element={<Notes />}/>
+      <Route path="/matieres" element={<Matieres />}/> */}
     </>
   )
 }

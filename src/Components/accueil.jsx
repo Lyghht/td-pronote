b17fb4194c3e538f55f5d12515d97eb0ccd0bf01
clément ALEXANDRE <clement.alexandre0207@gmail.com>
2024-05-15 11:46:39 +0200

    image accueil

--- src/Components/accueil.jsx
+++ src/Components/accueil.jsx
@@ -2,8 +2,9 @@
 function AccueilIUT() {
   return (
     <>
-        <div className="container">
+        <div className="container mt-4 mb-4 text-center">
             <h1>Bienvenue sur IUT-note</h1>
+            <img src="./src/assets/media/logo-IUT-blanc.png" alt="IUT" className="img-fluid" />
         </div>
     </>
   );
